49bbac24ae352b3ee544675d5ff28956a0459e0a
Leonardo Melo <32685264+leonardojaneis@users.noreply.github.com>
2021-03-15 18:53:44 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
 var port = process.env.PORT || 3000
-// SDK de Mercado Pago
-var mercadopago = require ('mercadopago');
-
-// Configura credenciais
-mercadopago.configure({
-  access_token: 'APP_USR-6096a634-0b35-452c-94c9-a18adb8ffb15'
-});
-
 var app = express();
  
 app.engine('handlebars', exphbs());
