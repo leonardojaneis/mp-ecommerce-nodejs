90f538d8b13b2f1236eec6a4a32cb2ef25b42b81
leonardo <leonardo@lanwork.com.br>
2021-03-19 20:55:22 -0300

    Loja com SDK MercadoPago

--- app.js
+++ app.js
@@ -3,85 +3,12 @@ var exphbs  = require('express-handlebars');
 var port = process.env.PORT || 3000
 var app = express();
 
-const mercadopago = require("mercadopago");
+//Colocar webhook dinâmico e enviar esta caralha pós o vídeo
+const PaymentController = require("./controller/PaymentController");
+const PaymentService = require("./classServ/PaymentService");
+const PaymentInstance = new PaymentController(new PaymentService());
 
-mercadopago.configurations.setAccessToken("APP_USR-334491433003961-030821-12d7475807d694b645722c1946d5ce5a-725736327"); 
-
-app.use(express.urlencoded({ extended: false})); //old true
 app.use(express.json());
-
-	
-
-app.post("/create_preference", (req, res) => {
-
-	
-
-	let preference = {
-		items: [{
-			id: "1234",
-			title: "Livrinho", 
-			description: "Celular de Tienda e-commerce",
-			picture_url: "https://agenciamoll.com.br/wp-content/uploads/2019/12/O-que-%C3%A9-URL-e-como-ela-Ajuda-na-sua-Estrat%C3%A9gia-Digital.jpg",
-			unit_price: 1500,            
-			quantity: 1,
-		}],
-		 payer: {
-		 	name: "Lalo",
-		 	surname: "Landa",
-		 	email: "test_user_92801501@testuser.com",
-		 	phone: {
-		 			area_code: "55",
-		 			number: 985298743
-		 	},
-		 	identification: {
-		 		type: "CPF",
-		 		number: "19119119100"
-		 	},
-		 	address: {
-		 			street_name: "Insurgentes Sur",
-		 			street_number: 1602,
-		 			zip_code: "78134-190"
-		 	},
-		 },
-
-		back_urls: {
-			"success": "http://localhost:8080/feedback",
-			"failure": "http://localhost:8080/feedback",
-			"pending": "http://localhost:8080/feedback"
-		},
-		auto_return: 'approved',
-		payment_methods: {
-				excluded_payment_methods: [
-					{
-						id: "amex"
-					}
-
-				],
-				installments: 6
-		},
-		notification_url: "https://webhook.site/ba74932a-af75-4806-8387-dae771cbe0af",
-		external_reference: "leonardojaneis@gmail.com"
-	
-	
-
-
-	};
-	/*
-	mercadopago.configure({
-	integrator_id: 'dev_24c65fb163bf11ea96500242ac130004'
-	});
-	*/
-
-	mercadopago.preferences.create(preference)
-		.then(function (response) {
-			res.json({id :response.body.id})
-		}).catch(function (error) {
-			console.log(error);
-		});
-});
-
-
-
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
@@ -93,6 +20,24 @@ app.use('/assets', express.static(__dirname + '/assets'));
 app.get('/', function (req, res) {
     res.render('home');
 });
+
+app.get("/success", (req, res) => {
+  res.render("success", req.query);
+});
+
+app.get("/error", (req, res) => {
+  res.render("error");
+});
+
+app.get("/pending", (req, res) => {
+  res.render("pending");
+});
+
+app.post("/payment/new", (req, res) =>
+  PaymentInstance.getMercadoPagoLink(req, res)
+);
+
+app.post("/webhook", (req, res) => PaymentInstance.webhook(req, res));
 
 app.get('/detail', function (req, res) {
     res.render('detail', req.query);
---
+++ classServ/PaymentService.js
@@ -0,0 +1,81 @@
+const axios = require("axios");
+
+class PaymentService {
+  constructor() {
+    this.tokensMercadoPago = {
+      prod: {},
+      test: {
+        access_token:
+          "APP_USR-334491433003961-030821-12d7475807d694b645722c1946d5ce5a-725736327"
+      }
+    };
+    this.mercadoPagoUrl = "https://api.mercadopago.com/checkout";
+  }
+
+  async createPaymentMercadoPago(name, price, unit, img) {
+    const url = `${this.mercadoPagoUrl}/preferences?access_token=${this.tokensMercadoPago.test.access_token}`;
+    img= 'https://leojaneis-mp-commerce-nodejs.herokuapp.com'+img.substring(1);
+    const items = [
+      {
+        id: 1234,
+        title: name,
+        description: "Celular de Tienda e-commerce",
+        picture_url: img,
+        quantity: 1,
+        unit_price: parseFloat(price)
+      }
+    ];
+
+    const preferences = {
+      items,
+      external_reference: "leonardojaneis@gmail.com",
+      payer: {
+        name: "Lalo",
+        surname: "Landa",
+        email: "test_user_92801501@testuser.com",
+        phone: {
+          area_code: "55",
+          number: "9852-98743"
+        },
+        identification: {
+        type: "CPF",
+        number: "19119119100"
+      },
+        address: {
+          zip_code: "78134-190",
+          street_name: "Insurgentes Sur",
+          street_number: "1602"
+        }
+      },
+      payment_methods: {
+        excluded_payment_methods: [
+          {
+            id: "amex"
+          }
+        ],
+        installments: 6,
+      },
+      back_urls: {
+        success: "https://leonardojaneis-mp-commerce-nodejs.herokuapp.com/success",
+        pending: "https://leojaneis-mp-commerce-nodejs.herokuapp.com/pending",
+        failure: "https://leonardojaneis-mp-commerce-nodejs.herokuapp.com/error"
+      },
+      notification_url: "https://webhook.site/ba74932a-af75-4806-8387-dae771cbe0af",
+      auto_return: "approved"
+    };
+
+    try {
+      const request = await axios.post(url, preferences, {
+        headers: {
+          "Content-Type": "application/json",
+          "x-integrator-id": "dev_24c65fb163bf11ea96500242ac130004"
+        }
+      });
+      return request.data;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+}
+
+module.exports = PaymentService;
