da3fa8bf1a539a33229ad4849c2371694b2df312
leonardo <leonardo@lanwork.com.br>
2021-03-19 21:39:00 -0300

    update

--- app.js
+++ app.js
@@ -42,4 +42,6 @@ app.get('/detail', function (req, res) {
     res.render('detail', req.query);
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
